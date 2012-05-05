7a7db30772933e80fc93a68bba32a18ce65a3193
Ripunjay Tripathi <ripunjay.tripathi@gmail.com>
2012-05-05 12:25:18 -0700

    First import

---
+++ scratchwork/sym.java
@@ -0,0 +1,13 @@
+
+Pckage Example;
+public class sym {
+  /* terminals */
+  public static final int RPAREN = 6;
+  public static final int error = 1;
+  public static final int PLUS = 3;
+  public static final int NUMBER = 7;
+  public static final int SEMI = 2;
+  public static final int LPAREN = 5;
+  public static final int TIMES = 4;
+  public static final int EOF = 0;
+}ATH=/usr/class/cs143/cool/bin:$PATH
